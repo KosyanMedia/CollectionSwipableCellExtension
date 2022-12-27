e0cf31e5356ed29a1ba2905344795034138a0a0d
Kattouf <vasiliy.kattouf@gmail.com>
2022-12-27 12:53:50 +0700

    Revert "rename public class API to avoit bug https://github.com/apple/swift/issues/56573"
    This reverts commit a814b627524f793b8fe046beb96749b4f6171bec.

--- Library/CollectionSwipableCellExtension.swift
+++ Library/CollectionSwipableCellExtension.swift
@@ -69,7 +69,7 @@ public protocol CollectionSwipableCellExtensionDelegate: AnyObject {
 }
 
 @objcMembers
-public class CollectionSwipableCellExtensionKit: NSObject {
+public class CollectionSwipableCellExtension: NSObject {
 
     /**
      Swipable extension delegate
@@ -152,7 +152,7 @@ public class CollectionSwipableCellExtensionKit: NSObject {
 
 // MARK: Public methods for programmatic control
 
-public extension CollectionSwipableCellExtensionKit {
+public extension CollectionSwipableCellExtension {
 
     /**
      Open actions for cell at specified index path
